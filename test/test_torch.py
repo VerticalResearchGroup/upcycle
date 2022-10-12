e47c39eb9645e9aae403f77376e3ecd04a764884
Michael Davies <michaelstoby@gmail.com>
2022-10-12 16:29:28 -0500

    update torch_Test

--- test/test_torch.py
+++ test/test_torch.py
@@ -1,10 +1,13 @@
 import upcycle as U
 
-app, trace, batch, dtype = U.apps.workload_factory('resnet50', 'offline', infer=True, layer=0)
+app, trace, batch, dtype = U.apps.workload_factory('resnet50', 'offline', infer=True)
 
-results = U.torchrun.run_with_torch(trace, device_type='cuda', device_id=0, niters=1000)
+results = U.torchrun.run_with_torch(trace, device_type='cuda', device_id=0, niters=100)
+
+a100_peak_fp16 = 312e12
 
 for op, lat_sec in zip(trace.oplist, results):
-    print(f'{op} : {result}')
+    op_util_a100 = op.flops / lat_sec / a100_peak_fp16
+    print(f'{op}:  \t\tavg. time={lat_sec:.5f}\tutil={op_util_a100 * 100:.2f}%')
 
 
